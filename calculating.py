86b423a65e36086684652b0d5502bb93f79f9bd1
Jirrer <jrirrer@gmail.com>
2026-04-19 19:51:44 -0400

    working on tracking

--- calculating.py
+++ calculating.py
@@ -3,27 +3,30 @@
 
 
 
-def processesCategories(filePath: str):
+def processesCategories(lines: list):
     categories = {}
 
-    with open(filePath, 'r', newline='') as file:
-        for row in file:
-            if not row: continue
+    for row in lines:
+        if not row: continue
 
-            splitRow = row.split(' ')
+        splitRow = row.split(' ')
 
-            action = splitRow[2]
+        action = splitRow[2]
 
-            date = splitRow[4]
-            year = date[:4]
+        date = splitRow[4]
+        year = date[:4]
 
-            time = splitRow[5].replace(':', '')
-            seconds = float(time)
+        time = splitRow[5].replace(':', '')
+        seconds = float(time)
 
-            if action in categories: categories[action] += 1
-            else: categories[action] = 1
+        if action in categories: categories[action] += 1
+        else: categories[action] = 1
 
     print(categories)
+
+
+def getDateCode(date):
+    return 109
 
 
 if __name__ == "__main__":
--- tracking.py
+++ tracking.py
@@ -2,12 +2,16 @@ import time, pywinctl, joblib
 from datetime import datetime
 from enum import Enum
 from idle_time import IdleMonitor
+import calculating
 
 
 # To-Do: model can learn from other assemenst (if final outcome is coding then train that checkWorkingWindow to be coding)
 # To-Do: maybe move assessments to another file
 
 workingWindowClassifier = joblib.load('classifiers\\window_title_classifier.joblib') 
+
+class FailedWritingNewLogFile(Exception): pass
+class FailedParsingLogFile(Exception): pass
 
 class Action(Enum):
     CODING = 'Coding'
@@ -53,14 +57,40 @@ def idleTime() -> bool:
 
     return False
     
-
+# To-Do: think of ways to hold logs in memory so i am not constally using disk
 def log(action: str, timestamp: datetime):
     try:
-        with open('logs.txt', 'a', newline='') as file: 
+        with open('logs.txt', 'a+', newline='') as file: 
             file.write(f'{action} - {timestamp}\n')
+
+            file.seek(0)
+
+            lines = file.readlines()
+
+        if len(lines) >= 1000:
+            refactorLogFile(lines)
 
     except Exception as e:
         print(f'Failed log: {e}')
+
+def refactorLogFile(lines: list):
+    try:
+        linesToRemove = lines[:500]
+        linesToKeep = lines[500:]
+
+        calculating.processesCategories(linesToRemove)
+
+    except Exception as e:
+        FailedParsingLogFile
+
+    try:
+        with open('logs.txt', 'w', newline='') as file:
+            for line in linesToKeep:
+                file.write(line)
+    
+    except Exception as e:
+        raise FailedWritingNewLogFile
+
 
 def checkWorkingWindow() -> bool:
     activeWindowTitle = pywinctl.getActiveWindowTitle()
